f48e3128496e9255a3e12ccbaf7370af76eb77ce
lancewburklin <1677@holbertonschool.com>
2020-04-07 15:37:37 +0000

    Moved to test folder

---
+++ Test/readline.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	ssize_t linesize;
+	char *buf = NULL;
+	size_t len = 0;
+
+	printf("$ ");
+	linesize = getline(&buf, &len, stdin);
+	if (buf == NULL)
+	{
+		printf("Failed\n");
+		free(buf);
+		return (-1);
+	}
+	printf("%s - %u\n", buf, (unsigned int)linesize);
+	free(buf);
+	return (1);
+}
