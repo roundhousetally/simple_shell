ad4f9479fcf707c12131f8528181215f828011bd
roundhousetally <tahlia.w.roper@gmail.com>
2020-04-08 20:52:21 +0000

    Get path workings

---
+++ getpath.c
@@ -0,0 +1,54 @@
+#include "pieshell.h"
+
+/**
+ * getpath - getting the path
+ *
+ *
+ *
+ */
+
+void getpath(char **s, char **envp)
+{
+	int i = 0, n = 0, count = 3;
+	char *setp = "PATH=";
+	char first[5];
+	char *passedpath;
+	int slen = _strlen(s[0]);
+
+	while (envp[i] != NULL)
+	{
+		n = 0;
+		while (n <= 4)
+		{
+			first[n] = envp[i][n];
+			n++;
+		}
+		first[n] = '\0';
+		if (_strcmp(first, setp) == 0)
+		{
+			while (envp[i][n] != '\0')
+			{
+				count++;
+				n++;
+			}
+
+			passedpath = malloc(sizeof(char) * count + slen);
+			if (passedpath == NULL)
+			{
+				printf("malloc error\n");
+				return;
+			}
+			n = 5;
+			count = 0;
+			while (envp[i][n] != '\0')
+			{
+				passedpath[count] = envp[i][n];
+				count++;
+				n++;
+			}
+			pathhelp(s, passedpath, envp);
+			free(passedpath);
+		}
+		i++;
+	}
+}
